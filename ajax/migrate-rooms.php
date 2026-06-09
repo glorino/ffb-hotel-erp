2d761f041b7c4386177c26685ce0b93f71aede6b
glorino <glorinomart@gmail.com>
2026-06-09 17:04:55 +0100

    Fix migrate-rooms.php - use correct includes

--- ajax/migrate-rooms.php
+++ ajax/migrate-rooms.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../config/constants.php';
+require_once __DIR__ . '/../includes/functions.php';
 
 header('Content-Type: application/json');
 
